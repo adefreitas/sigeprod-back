c90f8e99c70a7b492352e910ed2659818694fa01
adefreitas <adefreitasvz@outlook.com>
2015-05-28 19:22:24 -0430

    Tablas de materias y prelaciones creadas

---
+++ database/migrations/2015_05_28_234556_create_materias_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMateriasTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('materias', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->string('nombre');
+            $table->integer('creditos');
+            $table->integer('semestre');
+			$table->timestamps();
+		});
+
+        Schema::create('materias_prelaciones', function(Blueprint $table){
+            $table->integer('materia_id')->unsigned()->index();
+            $table->foreign('materia_id')->references('id')->on('materias')->onDelete('cascade');
+            $table->integer('prelacion_id')->unsigned()->index();
+            $table->foreign('prelacion_id')->references('id')->on('materias')->onDelete('cascade');
+        });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('materias');
+	}
+
+}
