5f1a651ffa196ba1c1dfca95799e0723db8acf8e
adefreitas <adefreitasvz@outlook.com>
2015-06-05 15:24:15 -0430

    Peque;os cambios

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,11 +14,13 @@
 use App\User;
 use Illuminate\Http\Response as HttpResponse;
 
+
 Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
-Route::post('/signup', function () {
+Route::post('/signup', function(){
+
     $credentials = Input::only('email', 'password');
 
     try {
@@ -30,23 +32,28 @@ Route::post('/signup', function () {
     $token = JWTAuth::fromUser($user);
 
     return Response::json(compact('token'));
+
 });
 
-Route::post('/signin', function () {
+Route::post('/signin', function(){
+
     $credentials = Input::only('email', 'password');
     $email = Input::only('email');
+
     if ( ! $token = JWTAuth::attempt($credentials)) {
         return Response::json(false, HttpResponse::HTTP_UNAUTHORIZED);
     }
+
     $user = User::where('email', $email)->first();
     $roles = $user->getRoles();
-    //var_dump($roles);
+
     return Response::json(compact('token', 'user', 'roles'));
+
 });
 
-Route::get('/restricted', [
-    'before' => 'jwt-auth',
-    function () {
+
+Route::get('/profile', ['before' => 'jwt-auth',
+    function(){
         $token = JWTAuth::getToken();
         $user = JWTAuth::toUser($token);
 
@@ -62,19 +69,3 @@ Route::get('/restricted', [
         ]);
     }
 ]);
-
-Route::group(['domain' => 'localhost', 'prefix' => 'api'], function () {
-    Route::get('/restricted', function () {
-        try {
-            JWTAuth::parseToken()->toUser();
-        } catch (Exception $e) {
-            return Response::json(['error' => $e->getMessage()], HttpResponse::HTTP_UNAUTHORIZED);
-        }
-
-        return ['data' => 'This has come from a dedicated API subdomain with restricted access.'];
-    });
-});
-//Route::controllers([
-//	'auth' => 'Auth\AuthController',
-//	'password' => 'Auth\PasswordController',
-//]);
