3a91f509af7eb5c772ae91ff13edc44af9e2aa02
danielhg7 <danielhg7@gmail.com>
2015-07-22 22:07:35 -0430

    Modificando las preferencias recibidas

--- app/Http/Controllers/PropositionController.php
+++ app/Http/Controllers/PropositionController.php
@@ -144,9 +144,67 @@ class PropositionController extends Controller {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function update($id)
+	public function update(Request $request, $id)
 	{
-		//
+		if($request->mode1[0]) {
+			$mode1 = 'Coordinador(a)';
+		}
+		elseif ($request->mode1[1]) {
+			$mode1 = 'Teoría';
+		}
+		elseif ($request->mode1[2]) {
+			$mode1 = 'Práctica';
+		}
+		elseif ($request->mode1[3]) {
+			$mode1 = 'Laboratorio';
+		}
+
+		if($request->mode2[0]) {
+			$mode2 = 'Coordinador(a)';
+		}
+		elseif ($request->mode2[1]) {
+			$mode2 = 'Teoría';
+		}
+		elseif ($request->mode2[2]) {
+			$mode2 = 'Práctica';
+		}
+		elseif ($request->mode[3]) {
+			$mode2 = 'Laboratorio';
+		}
+
+		if($request->mode3[0]) {
+			$mode3 = 'Coordinador(a)';
+		}
+		elseif ($request->mode3[1]) {
+			$mode3 = 'Teoría';
+		}
+		elseif ($request->mode3[2]) {
+			$mode3 = 'Práctica';
+		}
+		elseif ($request->mode3[3]) {
+			$mode3 = 'Laboratorio';
+		}
+
+		$proposition = Proposition::where('professor_id', $id)->update([
+			'course_option_1' => $request->course1,
+			'course_option_2' => $request->course2,
+			'course_option_3' => $request->course3,
+			'mode_option_1' => $mode1,
+			'mode_option_2' => $mode2,
+			'mode_option_3' => $mode3,
+			'schedule_1_option_1' => $request->schedule1[0],
+			'schedule_2_option_1' => $request->schedule1[1],
+			'schedule_1_option_2' => $request->schedule2[0],
+			'schedule_2_option_2' => $request->schedule2[1],
+			'schedule_1_option_3' => $request->schedule3[0],
+			'schedule_2_option_3' => $request->schedule3[1]
+			]);
+
+		return response()->json([
+
+				"msg" => "success"
+
+			]);
 	}
 
 	/**
