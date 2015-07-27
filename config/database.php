5732335fd6482d0eb06960b6cd79f99374c38dad
Andres De Freitas <adefreitasvz@outlook.com>
2015-07-27 15:01:57 -0430

    yolo

--- config/database.php
+++ config/database.php
@@ -3,12 +3,12 @@
 
 /* Esto es para heroku comentenlo en su version local */
 
-$url = parse_url(getenv("DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+// $url = parse_url(getenv("DATABASE_URL"));
+//
+// $host = $url["host"];
+// $username = $url["user"];
+// $password = $url["pass"];
+// $database = substr($url["path"], 1);
 
 /* fin de cosa para heroku 1 */
 
@@ -75,14 +75,14 @@ return [
 
 			/* Esto es para heroku, comenten esto y descomenten lo de arriba en su version local */
 
-			'driver'   => 'pgsql',
-	        'host'     => $host,
-	        'database' => $database,
-	        'username' => $username,
-	        'password' => $password,
-	        'charset'  => 'utf8',
-	        'prefix'   => '',
-	        'schema'   => 'public',
+			// 'driver'   => 'pgsql',
+	        // 'host'     => $host,
+	        // 'database' => $database,
+	        // 'username' => $username,
+	        // 'password' => $password,
+	        // 'charset'  => 'utf8',
+	        // 'prefix'   => '',
+	        // 'schema'   => 'public',
 
 			/* fin de cosa para heroku 2 */
 		],
