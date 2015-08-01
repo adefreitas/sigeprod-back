9337e8a5a163feeb94011e631cf608fff2c16af5
danielhg7 <danielhg7@gmail.com>
2015-08-01 19:16:19 -0430

    Agragando Materias

--- database/seeds/CourseTableSeeder.php
+++ database/seeds/CourseTableSeeder.php
@@ -24,66 +24,6 @@ class CourseTableSeeder extends Seeder {
         ************************* Materias ************************
         ***********************************************************/
 
-        Center::find(11)
-          ->courses()->save(
-            Course::create([
-                'id' => '6302',
-                'name' => 'Sistemas de Información',
-                'credits' => '5',
-                'type' => 'Obligatoria',
-                'semester' => '5',
-                'active' => true
-            ])
-        );
-
-        Center::find(22)
-          ->courses()->save(
-            Course::create([
-                'id' => '6003',
-                'name' => 'Comunicación de Datos',
-                'credits' => '6',
-                'type' => 'Obligatoria',
-                'semester' => '5',
-                'active' => true
-            ])
-        );
-
-        Center::find(33)
-          ->courses()->save(
-            Course::create([
-                'id' => '6303',
-                'name' => 'Bases de Datos',
-                'credits' => '5',
-                'type' => 'Obligatoria',
-                'semester' => '4',
-                'active' => true
-            ])
-        );
-
-        Center::find(44)
-          ->courses()->save(
-            Course::create([
-                'id' => '6211',
-                'name' => 'Interacción Humano-Computador',
-                'credits' => '5',
-                'type' => 'Electiva-Optativa',
-                'semester' => '6',
-                'active' => true
-            ])
-        );
-
-        Center::find(55)
-          ->courses()->save(
-            Course::create([
-                'id' => '6109',
-                'name' => 'Cálculo Científico',
-                'type' => 'Obligatoria',
-                'credits' => '6',
-                'semester' => '5',
-                'active' => true
-            ])
-        );
-
         Center::find(66)
           ->courses()->save(
             Course::create([
@@ -96,7 +36,7 @@ class CourseTableSeeder extends Seeder {
             ])
         );
 
-        Center::find(77)
+        Center::find(66)
           ->courses()->save(
             Course::create([
                 'id' => '6201',
@@ -132,6 +72,233 @@ class CourseTableSeeder extends Seeder {
             ])
         );
 
+        Center::find(44)
+          ->courses()->save(
+            Course::create([
+                'id' => '6001',
+                'name' => 'Organización y Estructura del Computador I',
+                'type' => 'Obligatoria',
+                'credits' => '6',
+                'semester' => '2',
+                'active' => true
+            ])
+        );
+
+        Center::find(66)
+          ->courses()->save(
+            Course::create([
+                'id' => '6107',
+                'name' => 'Matemáticas Discretas II',
+                'type' => 'Obligatoria',
+                'credits' => '4',
+                'semester' => '2',
+                'active' => true
+            ])
+        );
+
+        Center::find(44)
+          ->courses()->save(
+            Course::create([
+                'id' => '6002',
+                'name' => 'Organización y Estructura del Computador II',
+                'type' => 'Obligatoria',
+                'credits' => '5',
+                'semester' => '3',
+                'active' => true
+            ])
+        );
+
+        Center::find(44)
+          ->courses()->save(
+            Course::create([
+                'id' => '6203',
+                'name' => 'Ingeniería del Software',
+                'type' => 'Obligatoria',
+                'credits' => '5',
+                'semester' => '3',
+                'active' => true
+            ])
+        );
+
+        Center::find(77)
+          ->courses()->save(
+            Course::create([
+                'id' => '6108',
+                'name' => 'Matemáticas Discretas III',
+                'type' => 'Obligatoria',
+                'credits' => '4',
+                'semester' => '4',
+                'active' => true
+            ])
+        );
+
+        Center::find(44)
+          ->courses()->save(
+            Course::create([
+                'id' => '6004',
+                'name' => 'Sistemas Operativos',
+                'type' => 'Obligatoria',
+                'credits' => '6',
+                'semester' => '4',
+                'active' => true
+            ])
+        );
+
+        Center::find(33)
+          ->courses()->save(
+            Course::create([
+                'id' => '6303',
+                'name' => 'Bases de Datos',
+                'credits' => '5',
+                'type' => 'Obligatoria',
+                'semester' => '4',
+                'active' => true
+            ])
+        );
+
+        Center::find(77)
+          ->courses()->save(
+            Course::create([
+                'id' => '6104',
+                'name' => 'Probabilidad y Estadística',
+                'type' => 'Obligatoria',
+                'credits' => '5',
+                'semester' => '4',
+                'active' => true
+            ])
+        );
+
+        Center::find(11)
+          ->courses()->save(
+            Course::create([
+                'id' => '6003',
+                'name' => 'Comunicación de Datos',
+                'credits' => '6',
+                'type' => 'Obligatoria',
+                'semester' => '5',
+                'active' => true
+            ])
+        );
+
+        Center::find(33)
+          ->courses()->save(
+            Course::create([
+                'id' => '6302',
+                'name' => 'Sistemas de Información',
+                'credits' => '5',
+                'type' => 'Obligatoria',
+                'semester' => '5',
+                'active' => true
+            ])
+        );
+
+        Center::find(55)
+          ->courses()->save(
+            Course::create([
+                'id' => '6109',
+                'name' => 'Cálculo Científico',
+                'type' => 'Obligatoria',
+                'credits' => '6',
+                'semester' => '5',
+                'active' => true
+            ])
+        );
+
+        Center::find(55)
+          ->courses()->save(
+            Course::create([
+                'id' => '6204',
+                'name' => 'Lenguajes de Programación',
+                'type' => 'Obligatoria',
+                'credits' => '5',
+                'semester' => '5',
+                'active' => true
+            ])
+        );
+
+        Center::find(66)
+          ->courses()->save(
+            Course::create([
+                'id' => '6211',
+                'name' => 'Interacción Humano-Computador',
+                'credits' => '5',
+                'type' => 'Electiva-Optativa',
+                'semester' => '6',
+                'active' => true
+            ])
+        );
+
+        Center::find(66)
+          ->courses()->save(
+            Course::create([
+                'id' => '6221',
+                'name' => 'Aplicaciones con la Tecnología Internet',
+                'credits' => '5',
+                'type' => 'Electiva-Optativa',
+                'semester' => '6',
+                'active' => true
+            ])
+        );
+
+        Center::find(11)
+          ->courses()->save(
+            Course::create([
+                'id' => '6012',
+                'name' => 'Redes de Computadoras',
+                'credits' => '6',
+                'type' => 'Electiva-Optativa',
+                'semester' => '6',
+                'active' => true
+            ])
+        );
+
+        Center::find(33)
+          ->courses()->save(
+            Course::create([
+                'id' => '6311',
+                'name' => 'Administración de Bases de Datos',
+                'credits' => '5',
+                'type' => 'Electiva-Optativa',
+                'semester' => '6',
+                'active' => true
+            ])
+        );
+
+        Center::find(66)
+          ->courses()->save(
+            Course::create([
+                'id' => '6023',
+                'name' => 'Comercio Electrónico',
+                'credits' => '5',
+                'type' => 'Electiva',
+                'semester' => '7',
+                'active' => true
+            ])
+        );
+
+        Center::find(66)
+          ->courses()->save(
+            Course::create([
+                'id' => '6332',
+                'name' => 'Innovación Tecnológica',
+                'credits' => '5',
+                'type' => 'Electiva',
+                'semester' => '8',
+                'active' => true
+            ])
+        );
+
+        Center::find(44)
+          ->courses()->save(
+            Course::create([
+                'id' => '6241',
+                'name' => 'Desarrollo de Aplicaciones Distribuidas',
+                'credits' => '5',
+                'type' => 'Electiva',
+                'semester' => '7',
+                'active' => true
+            ])
+        );
 
 
         /**********************************************************
